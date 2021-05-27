3dc46c579a2a383e37ec3f2db6424ccab17d07ae
Patrick Cuadros Quiroga (Desarrollador) <pcuadros_dev@cmac.tacna>
2021-05-27 16:29:42 -0500

    v1

---
+++ lab_atdd_corebdd.domain/Calculadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lab_atdd_corebdd.domain
+{
+    public class Calculadora
+    {
+        public string Ecuacion { get; set; }
+        public int Total   { get; set; }
+        public int Sumar(int x, int y) => x + y;
+        public int Restar(int x, int y) => x - y;
+        public Calculadora Factor(int value)      { Total = value;  Ecuacion = value.ToString(); return this; }
+        public Calculadora Suma(int value)      { Total += value; Ecuacion += " + " + value;   return this; }
+        public Calculadora Resta(int value) { Total -= value; Ecuacion += " - " + value;   return this; }
+        public Calculadora Multiplica(int value) { Total *= value; Ecuacion += " x " + value;   return this; }
+        public Calculadora Divide(int value)   { Total /= value; Ecuacion += " / " + value;   return this; }
+        public Calculadora Limpiar()                 { Total = 0;      Ecuacion = "";               return this; }
+    }
+}
